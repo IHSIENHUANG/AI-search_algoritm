b45c646fa23e154e3a6a9fe9b4ed697f8da68763
I-HSIEN <isytkr34@gmail.com>
2018-03-20 15:54:49 -0700

    own version except skilearn

--- AI.py
+++ AI.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[217]:
+# In[94]:
 
 
 #purpose finish foward and backward search algorithm and create a high efficient search algorithm
@@ -14,7 +14,7 @@ import sys
 print ("this is my AI assginmant")
 
 
-# In[310]:
+# In[95]:
 
 
 def scale_normalized(data):
@@ -25,11 +25,11 @@ def scale_normalized(data):
     return data
 
 
-# In[311]:
+# In[104]:
 
 
 #dftrain = pd.read_csv("CS205_BIGtestdata__8.txt",delim_whitespace=True,header=None)
-dftrain = pd.read_csv("CS205_SMALLtestdata__68.txt",delim_whitespace=True,header=None)
+dftrain = pd.read_csv("CS205_SMALLtestdata__70.txt",delim_whitespace=True,header=None)
 #dftrain = pd.read_csv("CS205_SMALLtestdata__23.txt",delim_whitespace=True,header=None) #read the file and delimeter is whitespace
 # header is none is different with header is None
 total_features = dftrain.shape[1]#0 for row 1 for columns 
@@ -37,21 +37,41 @@ rows = dftrain.shape[0]
 print (dftrain.shape)
 labels = dftrain[dftrain.columns[0]]# keep the first column as labels
 print ("--start normalizing, and it will take some time")
-for i in range(1,total_features):
+for i in range(1,total_features):#the orocess of normalized
     dftrain[dftrain.columns[i]]=scale_normalized(dftrain[dftrain.columns[i]])
 print ("FINISH NORMALIZE")
 
 
-# In[364]:
+# In[105]:
 
 
-def leave_one_out_cross_validation(row,k,test_data):
-    X = np.reshape(test_data, (row, k))
-    for row in range(0,row):
-        print X[row,0:]
-        for other_row in range(0,row):
-            if other_row == row:
+def leave_one_out_cross_validation(rows,k,test_data):
+    X = np.reshape(test_data, (rows, k))
+    res = 0 
+    for row in range(0,rows):
+        distance_list = []#store each distance
+        label_index =-1
+        for other_row in range(0,rows):# compare each data with the rest data
+            distance = 0.0
+            if other_row == row:# do not need to compare with itself
+                distance_list.append(1000000.0)
                 continue
+            for (x,y) in zip (X[row,0:],X[other_row,0:]):# count the distance
+                distance += (x-y)**2
+            distance_list.append(distance)
+        min_distance = min(distance_list)
+        same_distance=0
+        for i,num in enumerate(distance_list):
+            if num == min_distance:
+                label_index = i#is is index num is number is distance_list
+        if labels[row] == labels[label_index]:#it means the prediction for this label is correct
+            res +=1
+    print ("The accuracy of data is :", res/100.00)
+    
+            
+                
+            
+            
             
 '''
 def leave_one_out_cross_validation(row,k,test_data):
@@ -78,7 +98,7 @@ def leave_one_out_cross_validation(row,k,test_data):
 '''
 
 
-# In[365]:
+# In[106]:
 
 
 def call_test_data(temp_items):
@@ -98,7 +118,7 @@ def call_test_data(temp_items):
     
 
 
-# In[366]:
+# In[107]:
 
 
 def forward_search(row,column):
@@ -139,7 +159,7 @@ def forward_search(row,column):
     
 
 
-# In[367]:
+# In[108]:
 
 
 def Backward_Elimination(row,column):
@@ -191,14 +211,14 @@ def Backward_Elimination(row,column):
     
 
 
-# In[368]:
+# In[110]:
 
 
 def main(): 
     print 'this is main function'
     #forward_search(rows,total_features)
     #Backward_Elimination(rows,total_features)
-    items = [1,2,3]
+    items = [8,4,1]
     test_data  = call_test_data(items)
     leave_one_out_cross_validation(100,3,test_data)
     #test_data()
