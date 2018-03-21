a61c57de1cb7707e4b0a04fd62df443227d475b7
I-HSIEN <isytkr34@gmail.com>
2018-03-20 23:02:36 -0700

    including own search

--- AI.py
+++ AI.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[94]:
+# In[ ]:
 
 
 #purpose finish foward and backward search algorithm and create a high efficient search algorithm
@@ -14,7 +14,7 @@ import sys
 print ("this is my AI assginmant")
 
 
-# In[95]:
+# In[ ]:
 
 
 def scale_normalized(data):
@@ -25,11 +25,11 @@ def scale_normalized(data):
     return data
 
 
-# In[104]:
+# In[222]:
 
 
-#dftrain = pd.read_csv("CS205_BIGtestdata__8.txt",delim_whitespace=True,header=None)
-dftrain = pd.read_csv("CS205_SMALLtestdata__70.txt",delim_whitespace=True,header=None)
+dftrain = pd.read_csv("CS205_BIGtestdata__8.txt",delim_whitespace=True,header=None)
+#dftrain = pd.read_csv("CS205_SMALLtestdata__70.txt",delim_whitespace=True,header=None)
 #dftrain = pd.read_csv("CS205_SMALLtestdata__23.txt",delim_whitespace=True,header=None) #read the file and delimeter is whitespace
 # header is none is different with header is None
 total_features = dftrain.shape[1]#0 for row 1 for columns 
@@ -42,7 +42,7 @@ for i in range(1,total_features):#the orocess of normalized
 print ("FINISH NORMALIZE")
 
 
-# In[105]:
+# In[223]:
 
 
 def leave_one_out_cross_validation(rows,k,test_data):
@@ -66,14 +66,9 @@ def leave_one_out_cross_validation(rows,k,test_data):
                 label_index = i#is is index num is number is distance_list
         if labels[row] == labels[label_index]:#it means the prediction for this label is correct
             res +=1
-    print ("The accuracy of data is :", res/100.00)
-    
-            
-                
-            
-            
-            
-'''
+    print ("The accuracy of data is :", res/100.00)    
+    return (res/100.00)
+'''this is the version i call by sklearn for practice 
 def leave_one_out_cross_validation(row,k,test_data):
     clf = neighbors.KNeighborsClassifier(n_neighbors=3, weights='uniform', algorithm='auto', leaf_size=30, p=2, metric='minkowski', metric_params=None,n_jobs=1)
     #neighbors.KNeighborsClassifier
@@ -98,7 +93,7 @@ def leave_one_out_cross_validation(row,k,test_data):
 '''
 
 
-# In[106]:
+# In[224]:
 
 
 def call_test_data(temp_items):
@@ -118,7 +113,7 @@ def call_test_data(temp_items):
     
 
 
-# In[107]:
+# In[225]:
 
 
 def forward_search(row,column):
@@ -138,10 +133,7 @@ def forward_search(row,column):
                 temp_items.append(i)
                 test_data = call_test_data(temp_items)# based on the element number to get new test_data columns 
                 cur_score = leave_one_out_cross_validation(row,num,test_data)# num is num of columns
-                sys.stdout.write(" accuracy is ")
-                sys.stdout.flush()
-                print cur_score
-                if cur_score >= max_score:
+                if cur_score > max_score:
                     new_element = i
                     max_score = cur_score
         print (max_score,new_element)
@@ -151,7 +143,6 @@ def forward_search(row,column):
     index = -1 # score the best features 
     for i, num in enumerate(lawyer_score):
         if num > max_num:
-            print(i)
             max_num = num
             index = i+1 
     print ("max score = " ,max_num)
@@ -159,7 +150,7 @@ def forward_search(row,column):
     
 
 
-# In[108]:
+# In[226]:
 
 
 def Backward_Elimination(row,column):
@@ -171,7 +162,8 @@ def Backward_Elimination(row,column):
     print (items)
     lawyer_score = []
     for num in range (1,total_features-1):
-        max_score = -1
+        max_score = -1 # store the best score in this lawyer
+        min_score = 101
         test_data = []
         new_element = -1
         for i in range (1,total_features):
@@ -184,10 +176,7 @@ def Backward_Elimination(row,column):
                 temp_items.remove(i)
                 test_data = call_test_data(temp_items)# based on the element number to get new test_data columns 
                 cur_score = leave_one_out_cross_validation(row,total_features-num-1,test_data)
-                sys.stdout.write(" accuracy is ")
-                sys.stdout.flush()
-                print cur_score
-                if cur_score >= max_score:
+                if cur_score > max_score:#delete the less
                     new_element = i
                     max_score = cur_score
         print (max_score,new_element)
@@ -197,7 +186,7 @@ def Backward_Elimination(row,column):
     max_num = 0 # score the maximum scores
     index = -1 # score the best features 
     for i, num in enumerate(lawyer_score):
-        if num > max_num:
+        if num >= max_num: # it can eliminate the feature as many as posiible 
             print(i)
             max_num = num
             index = i+1 
@@ -211,17 +200,93 @@ def Backward_Elimination(row,column):
     
 
 
-# In[110]:
+# In[227]:
+
+
+def original_leave_one_out_cross_validation(rows,k,test_data,max_score):
+    # the idea is that if we find the score is lower than before, do not need to finish the calculation
+    max_error = 100 - max_score*100.0
+    X = np.reshape(test_data, (rows, k))
+    error_rate = 0 
+    res = 0 
+    for row in range(0,rows):
+        if error_rate >= max_error:
+            print ("The accuracy of data is :", 0) 
+            return 0
+        distance_list = []#store each distance
+        label_index =-1
+        for other_row in range(0,rows):# compare each data with the rest data
+            distance = 0.0
+            if other_row == row:# do not need to compare with itself
+                distance_list.append(1000000.0)
+                continue
+            for (x,y) in zip (X[row,0:],X[other_row,0:]):# count the distance
+                distance += (x-y)**2
+            distance_list.append(distance)
+        min_distance = min(distance_list)
+        same_distance=0
+        for i,num in enumerate(distance_list):
+            if num == min_distance:
+                label_index = i#is is index num is number is distance_list
+        if labels[row] == labels[label_index]:#it means the prediction for this label is correct
+            res +=1
+        else:
+            error_rate +=1
+    print ("The accuracy of data is :", res/100.00)    
+    return (res/100.00)
+
+
+# In[228]:
+
+
+def original_search(row,total_features):
+    print ("--THIS IS MY OWN SEARCH--")
+    #basically, it is a forward search, but instead of leave_one_out_cross_validation this algorithm put concept of alpha 
+    #beta inside the the leave one out ->  original_leave_one_out_cross_validation
+    items=[]
+    lawyer_score = []
+    for num in range (1,total_features):
+        max_score = -1
+        test_data = []
+        new_element = -1
+        for i in range (1,total_features):
+            if i in items:
+                continue
+            else:
+                temp_items=[]
+                for element in items:# can not assgin temp_items = items, it will affect the items
+                    temp_items.append(element)
+                temp_items.append(i)
+                test_data = call_test_data(temp_items)# based on the element number to get new test_data columns 
+                cur_score = original_leave_one_out_cross_validation(row,num,test_data,max_score)# num is num of columns
+                if cur_score > max_score:
+                    new_element = i
+                    max_score = cur_score
+        print (max_score,new_element)
+        lawyer_score.append(max_score)
+        items.append(new_element)
+    max_num = 0 # score the maximum scores
+    index = -1 # score the best features 
+    for i, num in enumerate(lawyer_score):
+        if num > max_num:
+            max_num = num
+            index = i+1 
+    print ("max score = " ,max_num)
+    print ("feature = " ,items[0:index])
+
+
+# In[229]:
 
 
 def main(): 
     print 'this is main function'
     #forward_search(rows,total_features)
     #Backward_Elimination(rows,total_features)
-    items = [8,4,1]
-    test_data  = call_test_data(items)
-    leave_one_out_cross_validation(100,3,test_data)
-    #test_data()
+    original_search(rows,total_features)
+   # items = [8,4,1]
+    #test_data  = call_test_data(items)
+    #leave_one_out_cross_validation(100,3,test_data)
+
 
 if __name__ == "__main__":
     main()
