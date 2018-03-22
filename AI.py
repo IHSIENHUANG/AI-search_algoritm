7d7a114915b67d83392242c64aa1461e85d04cd7
I-HSIEN <isytkr34@gmail.com>
2018-03-22 10:31:00 -0700

    final version

--- AI.py
+++ AI.py
@@ -1,3 +1,9 @@
+
+# coding: utf-8
+
+# In[34]:
+
+
 #purpose finish foward and backward search algorithm and create a high efficient search algorithm
 import pandas as pd
 import numpy as np
@@ -5,16 +11,40 @@ import time
 from sklearn import preprocessing
 import sys
 print ("this is my AI assginmant")
+
+
+# In[35]:
+
+
 def scale_normalized(data):
     max_num = max(data)
     min_num = min(data)
     for i,item in enumerate(data):
         data[i] = (data[i]-min_num)/max_num
     return data
-#dftrain = pd.read_csv("CS205_BIGtestdata__8.txt",delim_whitespace=True,header=None)
-#dftrain = pd.read_csv("CS205_SMALLtestdata__70.txt",delim_whitespace=True,header=None)
-dftrain = pd.read_csv("CS205_SMALLtestdata__23.txt",delim_whitespace=True,header=None) #read the file and delimeter is whitespace
-#dftrain = pd.read_csv("CS205_SMALLtestdata__14.txt",delim_whitespace=True,header=None) #read the file and delimeter is whitespace
+
+
+# In[36]:
+
+
+dftrain = pd.read_csv("CS205_BIGtestdata__8.txt",delim_whitespace=True,header=None)
+print("choice of input")
+print("1 for the common data 68 provided by professor")
+print("2 for the common data 69 provided by professor")
+print("3 for the common data 70 provided by professor")
+print("4 for the individual small data 23  provided by professor")
+print("5 for the individual big data 8  provided by professor")
+test = raw_input()
+if int(test) ==1:
+    dftrain = pd.read_csv("CS205_SMALLtestdata__68.txt",delim_whitespace=True,header=None)
+elif int(test) ==2:
+    dftrain = pd.read_csv("CS205_SMALLtestdata__69.txt",delim_whitespace=True,header=None) #read the file and delimeter is whitespace
+elif int(test) ==3:
+    dftrain = pd.read_csv("CS205_SMALLtestdata__70.txt",delim_whitespace=True,header=None)
+elif int(test) ==4:
+    dftrain = pd.read_csv("CS205_SMALLtestdata__23.txt",delim_whitespace=True,header=None)
+elif int(test) ==5:
+    dftrain = pd.read_csv("CS205_BIGtestdata__8.txt",delim_whitespace=True,header=None)
 # header is none is different with header is None
 total_features = dftrain.shape[1]#0 for row 1 for columns 
 rows = dftrain.shape[0]
@@ -24,6 +54,11 @@ print ("--start normalizing, and it will take some time")
 for i in range(1,total_features):#the orocess of normalized
     dftrain[dftrain.columns[i]]=scale_normalized(dftrain[dftrain.columns[i]])
 print ("FINISH NORMALIZE")
+
+
+# In[37]:
+
+
 def leave_one_out_cross_validation(rows,k,test_data):
     res = 0 
     for i,item in enumerate(test_data):
@@ -48,6 +83,11 @@ def leave_one_out_cross_validation(rows,k,test_data):
             res +=1
     print ("The accuracy of data is :", res/100.00)
     return (res/100.00)
+
+
+# In[38]:
+
+
 def call_test_data(temp_items):
     data = []
     sys.stdout.write("using features: ")
@@ -61,6 +101,11 @@ def call_test_data(temp_items):
             temp.append(value[item])
         data.append(temp)
     return list(data)
+
+
+# In[39]:
+
+
 def forward_search(row,column):
     print ("start forward_search")
     items=[]
@@ -95,6 +140,11 @@ def forward_search(row,column):
     print ("max score = " ,max_num)
     print ("feature = " ,items[0:index])
     print ("total time is %s seconds",time.time()-start_time)
+
+
+# In[40]:
+
+
 def Backward_Elimination(row,column):
     print ("--start Backward_Elimination")
     start_time = time.time()
@@ -141,35 +191,49 @@ def Backward_Elimination(row,column):
         items.remove(item)
     print ("feature = " ,items)
     print ("total time is %s seconds",time.time()-start_time)
+
+
+# In[41]:
+
+
 def original_leave_one_out_cross_validation(rows,k,test_data,max_score):
     # the idea is that if we find the score is lower than before, do not need to finish the calculation
     max_error = 100 - max_score*100
-    error_rate = 0 
-    res = 0
-     
+    res = 0 
     for i,item in enumerate(test_data):
         min_distance = 100
+        error_rate = 0 
         distance_list = []
         index = -1
         for j,cmp_item in enumerate(test_data):
             distance =0.0
             if i==j:
-                distance_list.append(100.0)
+                distance_list.append([100.0,j])
                 continue
             if error_rate >= max_error:
                 print ("The accuracy of data is :", 0) 
                 return 0
             for (x,y) in zip(item,cmp_item):
                 distance += (x-y)**2
-            if distance < min_distance:
-                min_distance = distance
-                index = j
-        if labels[i] == labels[index]:#it means the prediction for this label is correct
+            distance_list.append([distance,j])
+        s = sorted(distance_list,reverse=False)
+        correct_num = 0 
+        for k,item in enumerate(s):
+            if labels[i]== labels[item[1]]:
+                correct_num+=1
+            if k ==2:#it means three nearest
+                break
+        if correct_num >=2:
             res +=1
         else:
             error_rate +=1
     print ("The accuracy of data is :", res/100.00)
     return (res/100.00)
+
+
+# In[42]:
+
+
 def original_search(row,total_features):
     print ("start recording time")
     start_time = time.time()
@@ -208,8 +272,44 @@ def original_search(row,total_features):
     print ("max score = " ,max_num)
     print ("feature = " ,items[0:index])
     print ("total time is %s seconds",time.time()-start_time)
+def own_search_greedy(row,total_features):
+    print ("start recording time")
+    start_time = time.time()
+    print ("--THIS IS MY OWN SEARCH with greedy version--")
+    items=[]
+    lawyer_score = []
+    score_for_init = []
+    max_score = 0 
+    index_time = 0.0
+    for num in range(1,total_features):
+        items = [num]
+        test_data = []
+        test_data = call_test_data(items)# based on the element number to get new test_data columns 
+        cur_score = original_leave_one_out_cross_validation(row,1,test_data,max_score)# num is num of columns
+        score_for_init.append([cur_score,num])# record each features with their score
+    s = sorted(score_for_init,reverse = True)
+    items=[]
+    for i,item in enumerate(s):
+        items.append(item[1])
+        test_data = []
+        test_data = call_test_data(items)# based on the element number to get new test_data columns 
+        cur_score = original_leave_one_out_cross_validation(row,i+1,test_data,max_score)# num is num of columns
+        lawyer_score.append(cur_score)
+    max_score = max(lawyer_score)
+    for i,item in enumerate(lawyer_score):
+        print (item)
+        if item == max_score:
+            print ("best score is %s",max_score)
+            print ("use features %s",items[0:i+1])
+            break
+    print("total use time %s",time.time()-start_time)
+
+
+# In[44]:
+
+
 def main(): 
-    print ("please input between 1-3 \n 1 for forward search \n 2 for backard_elemination \n 3 for original_search")
+    print ("please input between 1-3 \n 1 for forward search \n 2 for backard_elemination \n 3 for original_search\n 4 for greedy")
     choice = raw_input()
     if int(choice) == 1 :
         forward_search(rows,total_features)
@@ -217,7 +317,11 @@ def main():
         Backward_Elimination(rows,total_features)
     elif int(choice) == 3:
         original_search(rows,total_features)
+    elif int(choice) ==4:
+        own_search_greedy(rows,total_features)
     else:
         print ("please input betweeen 1-3")
 if __name__ == "__main__":
     main()
+
+
